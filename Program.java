ad66716fb27b4b9e1747c1b45c3f0eb99b0bd7d2
larissa-smith <94326675+larissa-smith@users.noreply.github.com>
2023-10-11 11:54:19 -0400

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,16 @@
+public class Program { 
+	public static void main(String [] args) {
+
+
+		// Append strings to output stream. 
+		System.out.append("cat");
+		System.out.append("\n");
+
+		// Append ranger of CharSequence. 
+		System.out.append("java program" , 5, 5+7);
+
+
+
+	}
+
+} 
